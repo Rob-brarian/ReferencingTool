4fd680e047aa3049d12f9c72253126935b5ecf81
Rob-brarian <150351529+Rob-brarian@users.noreply.github.com>
2024-05-27 11:37:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* global document, console, bootstrap */
-import reference_db from "./database/reference_db";
+import reference_db from "src/database/reference_db.js";
 
 //Initialise addon global variables
 const ref_DB = [...reference_db]; //copy database into addon global variable
